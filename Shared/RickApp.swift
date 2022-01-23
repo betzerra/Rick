c7d24f47aaee6cde52ca03ef36750f0f3475fbf3
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2022-01-23 14:40:08 -0300

    Initial Commit

---
+++ Shared/RickApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickApp.swift
+//  Shared
+//
+//  Created by Ezequiel Becerra on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct RickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
